5c3264e772da4fa2852143a2157a3f8e0250e874
Jowita Gogol <jmg88@bath.ac.uk>
2024-03-12 18:33:13 +0000

    Added CSVReader class

---
+++ src/main/java/org/library/utils/CSVReader.java
@@ -0,0 +1,38 @@
+package org.library.utils;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.library.models.Book;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+    public List<Book> readBooksFromCSV(String resourceFileName) {
+        List<Book> books = new ArrayList<>();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourceFileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
+
+            for (CSVRecord csvRecord : csvParser) {
+                // Accessing values by the names assigned to each column
+                int number = Integer.parseInt(csvRecord.get("Number"));
+                String title = csvRecord.get("Title");
+                String author = csvRecord.get("Author");
+                String genre = csvRecord.get("Genre");
+                String subGenre = csvRecord.get("SubGenre");
+                String publisher = csvRecord.get("Publisher");
+
+                Book book = new Book(number, title, author, genre, subGenre, publisher);
+                books.add(book);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+}
