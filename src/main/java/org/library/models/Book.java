60e730d8f0357617d8674a69cac6a85dbe3628c3
Jowita Gogol <jmg88@bath.ac.uk>
2024-03-14 17:33:15 +0000

    Added new variable to Book class to track the loan

--- src/main/java/org/library/models/Book.java
+++ src/main/java/org/library/models/Book.java
@@ -9,6 +9,7 @@ public class Book {
     private String publisher;
     private boolean isLoaned;
     private String loanedTo;
+    private int timesLoaned;
 
     public Book(int number, String title, String author, String genre, String subGenre, String publisher) {
         this.number = number;
@@ -19,6 +20,7 @@ public class Book {
         this.publisher = publisher;
         this.isLoaned = false;
         this.loanedTo = null;
+        this.timesLoaned = 0;
     }
 
     public Book() {
@@ -92,6 +94,26 @@ public class Book {
 
     public void setLoanedTo(String loanedTo) {
         this.loanedTo = loanedTo;
+    }
+
+    public boolean isLoaned() {
+        return isLoaned;
+    }
+
+    public void setLoaned(boolean loaned) {
+        isLoaned = loaned;
+    }
+
+    public int getTimesLoaned() {
+        return timesLoaned;
+    }
+
+    public void setTimesLoaned(int timesLoaned) {
+        this.timesLoaned = timesLoaned;
+    }
+
+    public void incrementLoanCounter() {
+        this.timesLoaned++;
     }
 
     @Override
