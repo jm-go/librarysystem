67263b2d3390571ad19a6ba8cdf98d0d414d069d
Jowita Gogol <jowita.m.gogol@gmail.com>
2024-03-20 22:30:46 +0000

    Updated User class

--- src/main/java/org/library/models/User.java
+++ src/main/java/org/library/models/User.java
@@ -1,14 +1,23 @@
 package org.library.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
     private String password;
     private String role;
+    private List<Integer> loanedBookNumbers;
 
     public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
+        this.loanedBookNumbers = new ArrayList<>();
+    }
+
+    // Default constructor
+    public User() {
     }
 
     public String getUsername() {
@@ -33,5 +42,17 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public List<Integer> getLoanedBookNumbers() {
+        return loanedBookNumbers;
+    }
+
+    public void setLoanedBookNumbers(List<Integer> loanedBookNumbers) {
+        this.loanedBookNumbers = loanedBookNumbers;
+    }
+
+    public void addLoanedBookNumber(int bookNumber) {
+        this.loanedBookNumbers.add(bookNumber);
     }
 }
